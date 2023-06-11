eda582e7786867dc8ad3994d327bb66ac2612076
Arun <arunattri09@gmail.com>
2023-06-11 14:10:20 +0530

    contact page improved

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,12 @@ import '../styles/Contact.css';
 
 function Contact() {
   return (
-    <div className="contact">
-      <h2>Contact Me</h2>
-      <p>Feel free to reach out to me through the following channels:</p>
-      <ul>
-        <li>
-          <strong>Phone:</strong> <a href="tel:+1234567890">+1 (234) 567-890</a>
-        </li>
-        <li>
-          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/arun-attri" target="_blank" rel="noopener noreferrer">Your LinkedIn Profile</a>
-        </li>
-        <li>
-          <strong>Instagram:</strong> <a href="https://www.instagram.com/your-instagram-profile" target="_blank" rel="noopener noreferrer">Your Instagram Profile</a>
-        </li>
+    <div class="social-container">
+      <ul class="social-icons">
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fa fa-github"></i></a></li>
       </ul>
     </div>
   );
